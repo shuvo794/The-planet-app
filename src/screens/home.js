707d3dae27e3115a54e8078beb67ec82828e868d
shuvo794 <shuvohossain794@gmail.com>
2022-05-24 08:35:44 +0600

    design done

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,15 +1,36 @@
-import { View, StyleSheet, SafeAreaView } from "react-native";
+import { View, StyleSheet, SafeAreaView, FlatList } from "react-native";
 import React from "react";
 import Text from "../components/text/text";
 
 import PlanetHeader from "../components/planet-header";
 import { colors } from "../theme/colors";
+import { PLANET_LIST } from "../data/planet-list";
 import { spacing } from "../theme/spacing";
-
+import { AntDesign } from "@expo/vector-icons";
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={PLANET_LIST}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => {
+          const { name, color } = item;
+          return (
+            <View style={styles.item}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <View style={[styles.circel, { backgroundColor: color }]} />
+                <Text preset="h4" style={styles.itemName}>
+                  {name}
+                </Text>
+              </View>
+              <AntDesign name="right" size={18} color="white" />
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={styles.separetor} />}
+      />
     </SafeAreaView>
   );
 }
@@ -17,5 +38,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
+  },
+  list: {
+    padding: spacing[4],
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: spacing[4],
+    justifyContent: "space-between",
+  },
+  itemName: {
+    textTransform: "uppercase",
+    marginLeft: spacing[4],
+  },
+  circel: {
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+  },
+  separetor: {
+    backgroundColor: colors.white,
+    borderWidth: 0.5,
   },
 });
